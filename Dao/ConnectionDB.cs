47b1f061cf2fd01c11fd9ff1ca42b37505bbacee
efren patricio galarza chavez <gefren@rgtdom.local>
2022-09-12 07:45:15 -0500

    conexion-basica

---
+++ Dao/ConnectionDB.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Odbc.Dao
+{
+    public class ConnectionDB
+    {
+        #region Atributos
+        private SqlConnection _connection;
+        #endregion
+
+
+        #region Constructores
+        public ConnectionDB(string cadenaConexion)
+        {
+            this._connection = new SqlConnection(cadenaConexion);
+        }
+        #endregion
+
+
+        #region Metodos
+        public SqlConnection Conectar()
+        {
+            try
+            {
+                //Habre la conexion
+                _connection.Open();
+                return _connection;
+            }
+            catch (SqlException se)
+            {
+                WriteLine($"Error capturado: {se.Message}");
+            }
+            catch (InvalidOperationException io)
+            {
+                WriteLine($"Error capturado: {io.Message}");
+            }
+            return null;
+        }
+
+
+        public void Desconectar()
+        {
+            try
+            {
+                //Cierra la conexion
+                _connection.Close();
+                //Libera los recursos
+                _connection.Dispose();
+            }
+            catch (SqlException se)
+            {
+                WriteLine($"Error capturado: {se.Message}");
+            }
+            finally
+            {   
+                _connection = null;
+            }
+        }
+        #endregion
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using static System.Console;
+using Odbc.Dao;
+using Microsoft.Data.SqlClient;
+using System.Security;
+
+namespace Odbc
+{
+    class Program
+    {
+        static void Main()
+        {
+            WriteLine("Conexion A Base de datos SQL Server");
+            var cone = new ConnectionDB("Data Source=localhost:1433;Initial Catalog=master;User ID=sa;Password=Adm1n2002;Application Name=usuario");
+            WriteLine(cone);
+
+
+        }
+    }
+}
